ac1606c6f8efeeae9baa33489696e1aecf11cdc0
Christopher Strawhacker <cwstrawhacker1@buffs.wtamu.edu>
2022-01-13 22:19:35 -0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CIDM3312
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            List<int> ListNew = new List<int>();
+
+            List<string> listOfstrings = new List<string>()  {"Soccer" , "Basketball", "football"};
+
+            listOfstrings.Add("Baseball");
+            listOfstrings.Add("Hockey");
+            listOfstrings.Add("Water Polo");
+                
+               foreach (string element in listOfstrings)
+            {
+                Console.WriteLine (element);
+            }
+
+        }
+    }
+}
